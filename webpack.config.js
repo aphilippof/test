e4d8cbb0053738159832aa5c7e06a9d3c3fa552d
aphilippof <aphilippof@gmail.com>
2017-09-19 09:20:52 +0400

    Create base structure

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+var webpack = require('webpack');
+webpack;
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/public'
+    }
+};
